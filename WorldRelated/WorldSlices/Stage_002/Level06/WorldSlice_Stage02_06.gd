ebb8430d0479a330ca51a9b3ff2fce8c64d512e8
tumat001 <castromatthieu23@gmail.com>
2024-03-01 15:55:46 +0800

    in the process of making 02_06.
    tested functionality, but no yet done with decors

--- WorldRelated/WorldSlices/Stage_002/Level06/WorldSlice_Stage02_06.gd
+++ WorldRelated/WorldSlices/Stage_002/Level06/WorldSlice_Stage02_06.gd
@@ -1,7 +1,17 @@
 extends "res://WorldRelated/AbstractWorldSlice.gd"
 
 
+
+const VIS_INS__ANIM_NAME__HIDE_ALL = "hide_all"
+const VIS_INS__ANIM_NAME__HIDE_EXCEPT_MOUSE = "hide_except_mouse"
+
+
 onready var portal_for_transition = $ObjectContainer/Portal
+
+onready var vis_ins_hide_hud = $MiscContainer/VisIns_HideHud
+onready var vis_ins_hide_hud_anim_sprite : AnimatedSprite = vis_ins_hide_hud.anim_sprite_of_vis_ins
+onready var vkp_hide_hud__all = $MiscContainer/VisIns_HideHud/VBoxContainer2/VKP_HideHud_All
+onready var vkp_hide_hud__except_mouse = $MiscContainer/VisIns_HideHud/VBoxContainer2/VKP_HideHud_ExceptForMouse
 
 #
 
@@ -10,7 +20,6 @@ func _init():
 	
 
 func as_test__override__do_insta_win():
-	#as_test__override__do_insta_win__template_capture_all_points()
 	StoreOfLevels.unlock_stage_03__and_start_at_stage_03_01_on_level_finish__if_appropriate()
 	game_elements.game_result_manager.end_game__as_win()
 	
@@ -40,6 +49,30 @@ func _on_wait_after_portal_enter_done():
 	
 	
 
+#
+
+func _ready() -> void:
+	vis_ins_hide_hud_anim_sprite.connect("animation_finished", self, "_on_vis_ins_anim_finished")
+	_swap_to_anim__hide_all()
+	
+	vkp_hide_hud__all.any_control_action_name = "toggle_hide_hud"
+	vkp_hide_hud__except_mouse.any_control_action_name = "toggle_focus_mode" 
 
 
+func _on_vis_ins_anim_finished():
+	if vis_ins_hide_hud_anim_sprite.animation == VIS_INS__ANIM_NAME__HIDE_ALL:
+		_swap_to_anim__hide_except_mouse()
+	else:
+		_swap_to_anim__hide_all()
+		
+
+func _swap_to_anim__hide_all():
+	vis_ins_hide_hud_anim_sprite.animation = VIS_INS__ANIM_NAME__HIDE_ALL
+	vkp_hide_hud__all.set_modulate_normal(Color("#F2F9BE"))
+	vkp_hide_hud__except_mouse.set_modulate_normal(Color("#55cfcfcf"))
+
+func _swap_to_anim__hide_except_mouse():
+	vis_ins_hide_hud_anim_sprite.animation = VIS_INS__ANIM_NAME__HIDE_EXCEPT_MOUSE
+	vkp_hide_hud__all.set_modulate_normal(Color("#55cfcfcf"))
+	vkp_hide_hud__except_mouse.set_modulate_normal(Color("#F2F9BE"))
 
